5e09cf7d177a0d5ba3309e09a81804da60a01a01
lecareli <leticia.careli@outlook.com>
2025-11-16 12:58:42 -0300

    Add Address and Store models with corresponding migrations; update User model to establish relationship with Store.

---
+++ app/Models/Store.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    protected $table = 'stores';
+    protected $fillable = [
+        'name',
+        'slug',
+        'website',
+        'phone',
+        'opening_hours',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'opening_hours' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_11_16_155104_create_stores_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('website')->nullable();
+            $table->string('phone', 15);
+            $table->json('opening_hours')->nullable();
+
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
